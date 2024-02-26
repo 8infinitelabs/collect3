a8380623a1943e3a2bdadc7b0562eb2fc2615783
Jhonny Varela <jhonny_varela_visbal@hotmail.com>
2024-02-26 15:49:24 -0400

    fixing bug in articles

--- packages/extension/src/pages/articles/articles.ts
+++ packages/extension/src/pages/articles/articles.ts
@@ -1,5 +1,5 @@
 import "./articles.css";
-import { getArticles, deleteArticle, listenToStorage } from "../../utils/storage";
+import { getArticles, deleteArticle, listenToStorage, getActiveStorage } from "../../utils/storage";
 import { Metadata } from "../../utils/utils";
 
 function articleToElement(key: string, article : Metadata) : HTMLLIElement {
@@ -31,70 +31,71 @@ function noElementMessage(container: Element) {
   container.appendChild(root);
 }
 
-getArticles().then((articles) => {
-  const container = document.querySelector(".fullclick");
-  const articlesArray = Array.from(articles.entries());
-  articlesArray.forEach(([key, metadata]) => {
-    container?.appendChild(articleToElement(key, metadata));
-  });
-  if (articlesArray.length) {
-    if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
-      container?.addEventListener('click', e => {
-        const target = e.target as Element;
-        if (target.nodeName === 'BUTTON') {
-          (target?.parentNode as Element).remove();
-          deleteArticle(target.id);
-        }
-      });
+getActiveStorage().then((storage) => {
+  getArticles().then((articles) => {
+    const container = document.querySelector(".fullclick");
+    const articlesArray = Array.from(articles.entries());
+    articlesArray.forEach(([key, metadata]) => {
+      container?.appendChild(articleToElement(key, metadata));
+    });
+    if (articlesArray.length) {
+      if (window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+        container?.addEventListener('click', e => {
+          const target = e.target as Element;
+          if (target.nodeName === 'BUTTON') {
+            (target?.parentNode as Element).remove();
+            deleteArticle(target.id);
+          }
+        });
+      } else {
+        container?.addEventListener('click', e => {
+          const target = e.target as Element;
+          if (target.nodeName === 'BUTTON') {
+            (target?.parentNode as HTMLElement).style.transition = '1s';
+            (target?.parentNode as Element).className = 'todelete';
+            deleteArticle(target.id);
+          }
+        });
+        container?.addEventListener('transitionend', e => {
+          const target = e.target as Element;
+          if (target.classList.contains('todelete')) {
+            target.remove();
+          }
+        });
+      }
     } else {
-      container?.addEventListener('click', e => {
-        const target = e.target as Element;
-        if (target.nodeName === 'BUTTON') {
-          (target?.parentNode as HTMLElement).style.transition = '1s';
-          (target?.parentNode as Element).className = 'todelete';
-          deleteArticle(target.id);
-        }
-      });
-      container?.addEventListener('transitionend', e => {
-        const target = e.target as Element;
-        if (target.classList.contains('todelete')) {
-          target.remove();
-        }
-      });
+      if (container) noElementMessage(container);
     }
-  } else {
-    if (container) noElementMessage(container);
-  }
-  listenToStorage((changes) => {
-    for (let [key, { oldValue: oldRawValue, newValue: newRawValue }] of Object.entries(changes)) {
-      if (key == "articles") {
-        const oldValue = new Map(JSON.parse(oldRawValue)) as Map<string, Metadata>;
-        const newValue = new Map(JSON.parse(newRawValue)) as Map<string, Metadata>;
-        if (oldValue.size == 0) {
-          const oldElement = container?.querySelector("#no-element");
-          oldElement?.remove();
-        }
-        if (newValue.size == 0) {
-          container!.innerHTML = "";
-          noElementMessage(container as Element);
-        } else if(oldValue.size < newValue.size){
-          newValue.forEach((value, key) => {
-            if (!oldValue.has(key)) {
-              container?.appendChild(articleToElement(key, value));
-            }
-          });
-        } else {
-          oldValue.forEach((_, key) => {
-            if (!newValue.has(key)) {
-              const oldElement = document.querySelector(`#${key}-root`);
-              oldElement?.remove();
-            } else {
-              console.log('newValue has key');
-            }
-          });
+    listenToStorage((changes) => {
+      for (let [key, { oldValue: oldRawValue, newValue: newRawValue }] of Object.entries(changes)) {
+        if (key === storage.url) {
+          const oldValue = new Map(JSON.parse(oldRawValue)) as Map<string, Metadata>;
+          const newValue = new Map(JSON.parse(newRawValue)) as Map<string, Metadata>;
+          if (oldValue.size == 0) {
+            const oldElement = container?.querySelector("#no-element");
+            oldElement?.remove();
+          }
+          if (newValue.size == 0) {
+            container!.innerHTML = "";
+            noElementMessage(container as Element);
+          } else if(oldValue.size < newValue.size){
+            newValue.forEach((value, key) => {
+              if (!oldValue.has(key)) {
+                container?.appendChild(articleToElement(key, value));
+              }
+            });
+          } else {
+            oldValue.forEach((_, key) => {
+              if (!newValue.has(key)) {
+                const oldElement = document.querySelector(`#${key}-root`);
+                oldElement?.remove();
+              } else {
+                console.log('newValue has key');
+              }
+            });
+          }
         }
       }
-    }
+    });
   });
-
 });
